e0829b84145691c15c9237d4fd0d74aeed4d07f6
kayo <osako.k.ab@m.titech.ac.jp>
2016-10-05 16:06:47 +0900

    hoge test2

--- app/src/main/java/jp/ac/titech/itpro/sdl/tsuyoso2/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/tsuyoso2/MainActivity.java
@@ -12,7 +12,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onResume(){}
+    protected void onResume(){
+        super.onResume();
+    }
 
     private void hoge(){
         //hoge hoge
